40bdeed173e0a6c4729402d780ca9f2514f45eb2
Amaan212 <toyota.supra.amaan@gmail.com>
2025-10-08 17:38:32 +0500

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,25 @@
+name ="penguin"
+age ="15"
+is_student=True
+weight=38.18
+print("Name :", name)
+print("Data Type of Name is",type(name))
+print("Age :", age)
+print("Data Type of Age is",type(age))
+print("is_student :",is_student)
+print("Data Type of is_student is",type(is_student))
+print("Weight :",weight)
+print("Data Type of Weight is",type(weight))
+weight=int(weight)
+print("Weight :",weight)
+print("Data Type of Weight is",type(weight))
+i=10
+q=20
+print("sum is", i+q)
+print("after type casting ",str(i)+str(q))
+example="Haa"*5
+print(example)
+myname="Amaan ishrat"
+print(myname)
+print(len(myname))
+print(myname[0]+"."+myname[6])
