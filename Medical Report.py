5a7391289f375cba0d0d7169d6e2f6d645aa103d
Amaan212 <toyota.supra.amaan@gmail.com>
2025-10-15 17:08:08 +0500

    Add files via upload

---
+++ Medical Report.py
@@ -0,0 +1,7 @@
+att= int(input("Enter number to check :"))
+m=input("You have medical a issue or not :")
+if att>75:
+    if m=="yes":
+     print("You are eligible to sit for the exam")
+else:
+    print("You can not sit for the exam")
