f168a7389321d2cc67b04eef29d78126aa10ce14
Amaan212 <toyota.supra.amaan@gmail.com>
2025-10-25 12:18:33 +0500

    Add files via upload

---
+++ Square turtle.py
@@ -0,0 +1,11 @@
+import turtle
+# creating canvas
+turtle.Screen().bgcolor("Orange")
+sc=turtle.Screen()
+sc.setup(400, 300)
+turtle.title("Welcome to turtle window")
+board =turtle.Turtle()
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+turtle.done()
