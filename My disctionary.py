8d6b7650678991bd55e43ebaf6aeefb0ad66935d
Amaan212 <toyota.supra.amaan@gmail.com>
2025-10-29 18:35:10 +0500

    Add files via upload

---
+++ My disctionary.py
@@ -0,0 +1,12 @@
+my_dict = {"name": "Amaan",1:[2,4,3]}
+my_dict = {'name': 'Amaan', 'age':12}
+print(my_dict['name'])
+print(my_dict['age'])
+my_dict['age'] = 14
+print(my_dict)
+my_dict['address'] = 'DownTown'
+print(my_dict)
+my_dict.pop('age')
+print(my_dict)
+my_dict.clear()
+print(my_dict)
