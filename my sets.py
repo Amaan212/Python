48a6a46449640a3d64b345fdb2666db2ac5546e0
Amaan212 <toyota.supra.amaan@gmail.com>
2025-10-31 14:17:38 +0500

    Add files via upload

---
+++ my sets.py
@@ -0,0 +1,8 @@
+my_set = {1,2,2,2,2,3,3,3,4,4}
+print("Set :", my_set)
+
+my_set1 = {2,4,5,6,8}
+print("Set :", my_set1)
+print("Union ", my_set.union(my_set1))
+print("Intersection ", my_set.intersection(my_set1))
+print("Difference ", my_set.difference(my_set1))
