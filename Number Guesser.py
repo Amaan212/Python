a6f5e5d616bc8a3f5f9ac4cd72af173aa787ea46
Amaan212 <toyota.supra.amaan@gmail.com>
2025-11-01 10:59:57 +0500

    Add files via upload

--- Number Guesser.py
+++ Number Guesser.py
@@ -1,8 +1,7 @@
 import random
 r = random.randint(1,30)
-input=int(input("Enter a Number between 1-30."))
-if r==21:
+myinput=int(input("Enter a Number between 1-30."))
+if r==myinput:
     print("You are correct!")
 else:
     print("Your answer is false")
-    print("Hint! It is a multiple of 7.")
